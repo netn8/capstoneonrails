b9b4dfdf6a9e5916fc5887fe265988e543955682
george <george@dringo.org>
2017-06-28 11:25:58 -0700

    moving along

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,16 @@ class HomeController < ApplicationController
   def show
     render template: "home"
   end
+  def show
+    render template: "engineering/mechanical"
+  end
+  def show
+    render template: "engineering/electrical"
+  end
+  def show
+    render template: "engineering/computer"
+  end
+  def show
+    render template: "contact"
+  end
 end
