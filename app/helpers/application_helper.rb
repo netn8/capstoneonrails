da88d4051a0ab5aaba1e409036bd0e87a094dcf9
George Nicholas <george@dringo.io>
2017-06-28 21:53:15 -0700

    ready for review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def dashboard_nav_link(text, path, classes)
+      @current_path = request.original_url
+    if @current_path.include?(path)
+      options = { class: "active " + classes }
+    else
+      options = { class: classes }
+    end
+    link_to text, path, options
+  end
+
 end
